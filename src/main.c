ebf45210122d29cb78c10643949b93fcb52d94f7
Nyxxie <nyxxie@protonmail.ch>
2016-05-29 00:40:09 -0500

    Removed udis86 in favor of capstone

--- src/main.c
+++ src/main.c
@@ -6,6 +6,10 @@
 
 #include <stdio.h>
 #include <argp.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#include <capstone/capstone.h>
 
 #include "config.h"
 
@@ -46,6 +50,18 @@ static error_t parse_opt(int key, char* arg, struct argp_state* state) {
 static struct argp argp = {options, parse_opt, 0, doc};
 
 /* Main function */
+char shellcode[] = {
+    "\x6a\x0b"                      /* push   0xb */
+    "\x58"                          /* pop    eax */
+    "\x31\xf6"                      /* xor    esi,esi */
+    "\x56"                          /* push   esi */
+    "\x68\x2f\x2f\x73\x68"          /* push   0x68732f2f */
+    "\x68\x2f\x62\x69\x6e"          /* push   0x6e69622f */
+    "\x89\xe3"                      /* mov    ebx,esp */
+    "\x31\xc9"                      /* xor    ecx,ecx */
+    "\x89\xca"                      /* mov    edx,ecx */
+    "\xcd\x80"                      /* int    0x80 */
+};
 int main(int argc, char *argv[]) {
 
     /* Create args */
@@ -60,7 +76,31 @@ int main(int argc, char *argv[]) {
         printf("FILE %s\n", arguments.file);
     }
 
-    printf("DO SHIT\n");
+
+
+
+
+    /* ------------ Shit ------------ */
+    csh handle;
+    if (cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK)
+        return -1;
+
+    cs_insn* insn;
+    size_t count = cs_disasm(handle,
+        shellcode, sizeof(shellcode)-1,
+        0x1000, 0, &insn);
+
+    if (count > 0) {
+        for (int i = 0; i < count; i++) {
+            printf("0x%x:\t%s\t%s\n", insn[i].address, insn[i].mnemonic,
+            insn[i].op_str);
+        }
+
+        cs_free(insn, count);
+    } else
+        printf("ERROR: Failed to disassemble given code!\n");
+
+    cs_close(&handle);
 
     return 0;
 }
