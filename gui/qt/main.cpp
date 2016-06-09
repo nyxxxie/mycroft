4ced4ffbc522fb0ec8814fea7c2952ab268bf44d
Nyxxie <nyxxie@protonmail.ch>
2016-06-08 19:54:58 -0500

    No progress on the gui, just added some code to play around with.

--- gui/qt/main.cpp
+++ gui/qt/main.cpp
@@ -1,11 +1,51 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QTextEdit>
+#include <QSplitter>
+#include <QVBoxLayout>
+#include <QWidget>
+
 
 int main(int argc, char *argv[])
 {
+//    QApplication a(argc, argv);
+//    MainWindow w;
+//    w.show();
+
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    QWidget wnd;
+
+    QTextEdit* editor1 = new QTextEdit;
+    editor1->setText("Editor1");
+    QTextEdit* editor2 = new QTextEdit;
+    editor2->setText("Editor2");
+
+    QSplitter* split1 = new QSplitter;
+    split1->setOrientation(Qt::Vertical);
+    split1->addWidget(editor1);
+    split1->addWidget(editor2);
+
+    QVBoxLayout* container_editors = new QVBoxLayout;
+    container_editors->setContentsMargins(0,0,0,0);
+    container_editors->addWidget(split1);
+
+    QWidget* container = new QWidget;
+    container->setLayout(container_editors);
+
+    QSplitter* split2 = new QSplitter;
+    QTextEdit* editor3 = new QTextEdit;
+    editor3->setText("Editor3");
+
+    split2->addWidget(editor3);
+    split2->addWidget(container);
+
+    QVBoxLayout* layout_wnd = new QVBoxLayout;
+    layout_wnd->setContentsMargins(0,0,0,0);
+    layout_wnd->addWidget(split2);
+
+    wnd.setLayout(layout_wnd);
+    wnd.show();
 
     return a.exec();
 }
