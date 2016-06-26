68476a5c439b44de15e9fc39d7370563665de037
Nyxxie <nyxxie@protonmail.ch>
2016-06-25 22:44:50 -0500

    Added more to the file library and implemented some of the functions.

--- src/core/file.c
+++ src/core/file.c
@@ -1,46 +1,55 @@
 #include <stdio.h>
+#include <string.h>
 #include "file.h"
 
 int file_exists(const char* file_name) {
 
     FILE* fp;
     if ((fp = fopen(file_name, "r")) != NULL) {
-        // file exists
-        printf("It does exist.\n");
         fclose(fp);
-        return 0;
+        return 0; //File exists
     }
     else {
-        //File not found
-        printf("It doesn't exist.\n");
-        return -1;
+        return -1; //File not found
     }
 }
 
-int file_init(mycroft_file_t* file) {
-    //char*    name;  //TODO: populate this var and the one below
-    //char*    path;
+int file_init(mycroft_file_t* file, const char* file_name) {
+
     file->fp = NULL;
     file->size = 0;
-    //file->base = 0;
-    //file->cursor = 0;
+    file->cursor = 0;
+
+    file->path = (char*)malloc(strlen(file_name));
+    if (file->path == NULL) {
+        return -1;
+    }
+    strcpy(file->path, file_name);
+
+    file->name = file->path;  //TODO: figure out how to get basename
+
+    return 0;
 }
 
 int file_open(mycroft_file_t* file, const char* file_name) {
 
-    //TODO: try and figure out how to check if the file exists and open it
-    //      in a neater fashion?
-    //      freopen might be a good candidate...
+    if (file_name == 0 || strlen(file_name) == 0) {
+        return -1;
+    }
 
-    /* Check if file exists */
-    if (file_exists(file_name) < 0) {
+    if (file_init(file, file_name) < 0) {
         return -1;
     }
 
     /* Try to open file */
-    if ((file->fp = fopen(file_name, "ab+")) == NULL) {
+    if ((file->fp = fopen(file->path, "rb+")) == NULL) {
         return -1;
     }
+
+    /* Copy file name and path */
+    int nsize = strlen(file_name);
+    file->path = (char*)malloc(nsize + 1);
+    strcpy(file->path, file_name);
 
     /* Get file size */
     fseek(file->fp, 0, SEEK_END);
@@ -54,108 +63,122 @@ int file_close(mycroft_file_t* file) {
     if (file->fp != NULL) {
         fclose(file->fp);
     }
+
+    return 0;
 }
 
-int file_read_value(mycroft_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf) {
+int file_get_cursor(mycroft_file_t* file) {
+    //file->cursor = ftell(file->fp);
+    return file->cursor;
+}
+
+int file_set_cursor(mycroft_file_t* file, int cursor) {
+
+    if (cursor > file->size) {
+        fprintf(stderr, "Tried to set cursor past the end of the file.");
+        return -1;
+    }
+
+    int ret = fseek(file->fp, cursor, SEEK_SET);
+    if (ret < 0) {
+        perror("DEBUG: Error in file_set_cursor: ");
+        return -1;
+    }
+
+    file->cursor = cursor;
+
+    return 0;
+}
+
+fsize_t file_size(mycroft_file_t* file) {
+    return file->size;
+}
+
+int file_read_pos(mycroft_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf) {
 
     /* Make sure that we don't read off the end of the file */
-    if ((offset + amount) <= file->size) {
+    if ((offset + amount) >= file->size) {
         fprintf(stderr, "Tried to read off the end of the file.\n");
         return -1;
     }
 
-    /* Save current position */
-    //TODO: probably could just file->cursor value
-    fpos_t pos;
-    fgetpos(file->fp, &pos);
+    //TODO: check to see if we can read from editor_buffer's buf?
 
-    /* Set position and REED */
-    fseek(file->fp, 9, SEEK_SET);
-    long int res = fread(outbuf, 1, offset+amount, file->fp);
-    if (res != offset+amount) {
-        fprintf(stderr, "Didn't read all of the requested byes for some reason.\n");
+    /* Save old position and move to offset */
+    fsize_t old_cursor = file->cursor;
+    if (file_set_cursor(file, offset) < 0) {
+        perror("DEBUG: Error in file_set_cursor: ");
         return -1;
+    }
+
+    /* Read desired content */
+    long int res = fread(outbuf, 1, amount, file->fp);
+    if (res != amount) {
+        if (ferror(file->fp)) {
+            perror("DEBUG: Error in file_write_pos");
+            return -1;
+        }
+        else if (feof(file->fp)) {
+            fprintf(stderr, "Tried to read past end of file.\n");
+            return -1;
+        }
+        else {
+            fprintf(stderr, "fread failed for some unknown reason.  Report this!\n");
+            return -1;
+        }
     }
 
     /* Set original position */
-    fsetpos(file->fp, &pos);
+    if (file_set_cursor(file, old_cursor) < 0) {
+        perror("DEBUG: Error in file_set_cursor: ");
+        return -1;
+    }
+
+    return 0;
 }
 
-int file_write_value(mycroft_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf) {
+int file_write_pos(mycroft_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf) {
 
-    /* Make sure that we don't read off the end of the file */
-    if ((offset + amount) <= file->size) {
-        fprintf(stderr, "Tried to read off the end of the file.\n");
+    /* Make sure that we don't write past file contents */
+    if ((offset + amount) >= file->size) {
+        fprintf(stderr, "Tried to write off the end of the file.\n");
         return -1;
     }
 
-    /* Save current position */
-    //TODO: probably could just file->cursor value
-    fpos_t pos;
-    fgetpos(file->fp, &pos);
-
-    /* Set position and write */
-    fseek(file->fp, 9, SEEK_SET);
-    long int res = fwrite(outbuf, 1, offset+amount, file->fp);
-    if (res != offset+amount) {
-        fprintf(stderr, "Didn't write all of the requested byes for some reason.\n");
+    /* Save old position and move to offset */
+    fsize_t old_cursor = file->cursor;
+    if (file_set_cursor(file, offset) < 0) {
         return -1;
     }
+
+    printf("cursor PRE = %i\n", ftell(file->fp));
+
+    /* Read desired content */
+    long int res = fwrite(outbuf, 1, amount, file->fp);
+    if (res != amount) {
+        if (ferror(file->fp)) {
+            perror("DEBUG: Error in file_write_pos");
+            return -1;
+        }
+        else if (feof(file->fp)) {
+            // Reached eof (HOW??)
+            return -1;
+        }
+        else {
+            fprintf(stderr, "fwrite failed for some unknown reason.  Report this!\n");
+            return -1;
+        }
+    }
+
+    printf("cursor PST = %i\n", ftell(file->fp));
 
     /* Set original position */
-    fsetpos(file->fp, &pos);
+    if (file_set_cursor(file, old_cursor) < 0) {
+        perror("DEBUG: Error in file_set_cursor");
+        return -1;
+    }
+
+    return 0;
 }
 
-int file_read_editor_area(mycroft_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf) {
-
-    //TODO: make this use editor_buffer_t
-
-    /* Make sure that we don't read off the end of the file */
-    if ((offset + amount) <= file->size) {
-        fprintf(stderr, "Tried to read off the end of the file.\n");
-        return -1;
-    }
-
-    /* Save current position */
-    //TODO: probably could just file->cursor value
-    fpos_t pos;
-    fgetpos(file->fp, &pos);
-
-    /* Set position and REED */
-    fseek(file->fp, 9, SEEK_SET);
-    long int res = fread(outbuf, 1, offset+amount, file->fp);
-    if (res != offset+amount) {
-        fprintf(stderr, "Didn't read all of the requested byes for some reason.\n");
-        return -1;
-    }
-
-    /* Set original position */
-    fsetpos(file->fp, &pos);
-}
-
-int file_write_editor_area(mycroft_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf) {
-
-    //TODO: make this use editor_buffer_t
-
-    /* Make sure that we don't read off the end of the file */
-    if ((offset + amount) <= file->size) {
-        fprintf(stderr, "Tried to read off the end of the file.\n");
-        return -1;
-    }
-
-    /* Save current position */
-    //TODO: probably could just file->cursor value
-    fpos_t pos;
-    fgetpos(file->fp, &pos);
-
-    /* Set position and write */
-    fseek(file->fp, 9, SEEK_SET);
-    long int res = fwrite(outbuf, 1, offset+amount, file->fp);
-    if (res != offset+amount) {
-        fprintf(stderr, "Didn't write all of the requested byes for some reason.\n");
-        return -1;
-    }
-
-    /* Set original position */
-    fsetpos(file->fp, &pos);
-}
+//TODO: implement the rest of these functions
--- src/core/file.h
+++ src/core/file.h
@@ -17,19 +17,20 @@ typedef long int fsize_t;
  */
 typedef struct {
     uint8_t* buf;
-    fsize_t size;
-    fsize_t base;
-    fsize_t cursor; //TODO: change this as needed to work with the read/write editor area functions
+    fsize_t  size;
+    fsize_t  base;
+    fsize_t  cursor; //TODO: change this as needed to work with the read/write editor area functions
 } editor_buffer_t;
 
 /** Contains info for an open file */
 typedef struct {
 
     /* File info */
-    char*    name;  //TODO: populate this var and the one below
-    char*    path;
-    FILE*    fp;
-    fsize_t  size;
+    char*   name;  //TODO: populate this var and the one below
+    char*   path;
+    FILE*   fp;
+    fsize_t size;
+    fsize_t cursor;
 
     editor_buffer_t buf;
 } mycroft_file_t;
@@ -38,14 +39,23 @@ typedef struct {
 int file_exists(const char* file);
 
 /* Init/free functions */
+int file_init(mycroft_file_t* file, const char* file_name);
 int file_open(mycroft_file_t* file, const char* file_name);
 int file_close(mycroft_file_t* file);
 
+/* Management functions */
+int file_get_cursor(mycroft_file_t* file);
+int file_set_cursor(mycroft_file_t* file, int cursor);
+fsize_t file_size(mycroft_file_t* file);
+
 /* Read/write functions */
-int file_read_value(mycroft_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf);
-int file_write_value(mycroft_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf);
+int file_read(mycroft_file_t* file, fsize_t amount, uint8_t* outbuf);
+int file_read_pos(mycroft_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf);
 int file_read_editor_area(mycroft_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf);
+int file_write(mycroft_file_t* file, fsize_t amount, uint8_t* outbuf);
+int file_write_pos(mycroft_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf);
 int file_write_editor_area(mycroft_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf);
+int file_write_insert(mycroft_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf);
 
 #ifdef __cplusplus
 }
