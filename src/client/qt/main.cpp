d322397b5326ef515f1576a8570a89ac3a0350e9
Nyxxie <nyxxie@protonmail.ch>
2016-07-16 17:07:52 -0500

    Added command line option parsing.

--- src/client/qt/main.cpp
+++ src/client/qt/main.cpp
@@ -1,12 +1,41 @@
+#include <stdio.h>
 #include <QApplication>
+#include <QCommandLineParser>
+#include <mycroft/mycroft.h>
 #include "mainwindow.h"
 
-int main(int argc, char *argv[])
-{
-    //TODO: Set up argument parser
+int main(int argc, char *argv[]) {
 
     QApplication a(argc, argv);
+
+    /* Set up command line argument parser */
+    QCommandLineParser parser;
+    parser.setApplicationDescription("qt client for mycroft, a framework for rapid file format reverse engineering.");
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    /* Add command line arguments */
+    parser.addPositionalArgument("target", "File to play with.");
+
+    /* Process parser arguments */
+    parser.process(a);
+    const QStringList args = parser.positionalArguments();
+
+    QString filename = args.at(0);
+
+    /* Check if we were given a file */
+    if (filename.size() == 0) {
+        printf("You didn't specify a file to open, please do that.\n");
+        return 1;
+    }
+
+    printf("File: %s\n", filename.toStdString().c_str());
+
+    /* Create mycroft instance */
+
+    /* Create main window */
     MainWindow w;
     w.show();
+
     return a.exec();
 }
