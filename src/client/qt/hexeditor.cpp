d640dd2b88aed4f28275a1569c4328a261683075
Nyxxie <nyxxie@protonmail.ch>
2016-07-17 21:13:13 -0500

    Added basic hex display capabilities to the hexeditor widget

--- src/client/qt/hexeditor.cpp
+++ src/client/qt/hexeditor.cpp
@@ -1,24 +1,174 @@
+#include <QAbstractScrollArea>
+#include <QScrollBar>
 #include <QPainter>
 #include <QColor>
+#include <mycroft/mdb.h>
+#include <mycroft/file.h>
+#include <stdio.h>
+#include "mainwindow.h"
 #include "hexeditor.h"
 
 void HexEditor::init() {
-    setFont(QFont("Courier", 10));
+
+    curfile = NULL;
+    element_offset = 0;
+    element_gap = QMC_HEXEDIT_ELEMENT_GAP;
+    rows_shown = 0;
+    font_cwidth = 0;
+    font_cheight = 0;
+    text_offset = 3;
+    row_offset = 4;
+    addrbar_offset = 0;
+    addrbar_width = 0;
+    addrbar_num_max = 0;
+    hexarea_offset = 0;
+    hexarea_width = 0;
+    hexarea_text_gap = 3;
+    asciiarea_offset = 0;
+    asciiarea_width = 0;
+    textarea_width = 0;
+    textarea_rows = 0;
+
+    setFont(QFont("Courier", 11));
+}
+
+void HexEditor::adjust() {
+
+    textarea_width = 2*element_gap + addrbar_width + hexarea_width + asciiarea_width;
+
+    horizontalScrollBar()->setRange(0, textarea_width - viewport()->width());
+    horizontalScrollBar()->setPageStep(viewport()->width());
+    //verticalScrollBar()->setRange();
+    //verticalScrollBar()->setPageStep(viewport->());
+}
+
+void HexEditor::setCurrentFile(mc_file_t* file) {
+
+    curfile = file;
+
+    /* Calc addressbar width */
+    addrbar_num_max = QString("%1").arg(QString::number(file_size(curfile), 16)).size();
+    addrbar_width = ((addrbar_num_max+1)*font_cwidth) + (2*text_offset);
 }
 
 void HexEditor::setFont(const QFont& font) {
 
     QWidget::setFont(font);
-    //TODO: figure out font-related size info here
+
+    QFontMetrics fm(font);
+    font_cwidth = fm.width(' ');
+    font_cheight = fm.height() / 2;
+
+    /* Calc hexarea width */
+    hexarea_width = QMC_HEXEDIT_BYTESPERROW * (2*font_cwidth)
+        + (QMC_HEXEDIT_BYTESPERROW-1) * hexarea_text_gap
+        + (2*text_offset);
+
+    /* Calc asciiarea width */
+    asciiarea_width = (QMC_HEXEDIT_BYTESPERROW*font_cwidth) + (2*text_offset);
+
     viewport()->update();
 }
 
-HexEditor::HexEditor() {
-    init();
+void HexEditor::drawNoFile(QPainter& painter) {
+    painter.drawText(geometry(),
+        Qt::AlignHCenter|Qt::AlignVCenter,
+        "No file loaded.");
+}
+
+void HexEditor::drawAddressBar(QPainter& painter) {
+
+    addrbar_offset = element_offset;
+
+    /* Draw bar part */
+    painter.fillRect(
+        QRect(element_offset, 0, addrbar_width, viewport()->height()),
+        QColor(255, 0, 0));
+
+    /* Draw address part */
+    for (int i=1, addr=0; addr <= file_size(curfile); i++, addr += QMC_HEXEDIT_BYTESPERROW) {
+        painter.drawText(element_offset+text_offset,
+            i*(font_cheight+row_offset),
+            QString("%1h").arg(addr, addrbar_num_max, 16, QChar('0')).toUpper());
+    }
+
+    element_offset += addrbar_width + element_gap;
+}
+void HexEditor::drawHexContent(QPainter& painter) {
+
+    hexarea_offset = element_offset;
+
+    /* Draw bar part */
+    painter.fillRect(
+        QRect(element_offset, 0, hexarea_width, viewport()->height()),
+        QColor(0, 255, 0));
+
+    /* Draw bytes */
+    int cursor_prev = file_get_cursor(curfile);
+    for (int row=1 ;; row++) {
+
+        /* Read file bytes */
+        uint8_t bytes[QMC_HEXEDIT_BYTESPERROW];
+        int amnt = file_read(curfile, QMC_HEXEDIT_BYTESPERROW, bytes);
+        if (amnt == 0) {
+            break;
+        }
+        else if (amnt < 0) {
+            return;
+        }
+
+        /* Display bytes */
+        int offset = text_offset;
+        for (int j=0; j < amnt; j++) {
+            painter.drawText(offset+element_offset+(j*font_cwidth*2),
+                row*(font_cheight+row_offset),
+                QString("%1").arg(bytes[j], 2, 16, QChar('0')).toUpper());
+            offset += hexarea_text_gap;
+        }
+    }
+    file_set_cursor(curfile, cursor_prev);
+
+    element_offset += hexarea_width + element_gap;
+}
+
+void HexEditor::drawAsciiContent(QPainter& painter) {
+
+    asciiarea_offset = element_offset;
+
+    /* Draw bar part */
+    painter.fillRect(
+        QRect(element_offset, 0, asciiarea_width, viewport()->height()),
+        QColor(0, 0, 255));
+
+    /* Draw bytes */
+    int cursor_prev = file_get_cursor(curfile);
+    for (int row=1 ;; row++) {
+
+        /* Read file bytes */
+        uint8_t bytes[QMC_HEXEDIT_BYTESPERROW];
+        int amnt = file_read(curfile, QMC_HEXEDIT_BYTESPERROW, bytes);
+        if (amnt == 0) {
+            break;
+        }
+        else if (amnt < 0) {
+            return;
+        }
+
+        /* Display bytes */
+        for (int j=0; j < amnt; j++) {
+            painter.drawText(text_offset+element_offset+(j*font_cwidth),
+                row*(font_cheight+row_offset),
+                QString("%1").arg(QChar(bytes[j])));
+        }
+    }
+    file_set_cursor(curfile, cursor_prev);
+
+    element_offset += asciiarea_width + element_gap;
 }
 
 void HexEditor::keyPressEvent(QKeyEvent *event) {
-
+    // TODO: for experimentation purposes, print these events in a qt status
+    //       bar (we need to add one anyways)
 }
 
 void HexEditor::mouseMoveEvent(QMouseEvent * event) {
@@ -33,10 +183,33 @@ void HexEditor::paintEvent(QPaintEvent *event) {
 
     QPainter painter(viewport());
 
-    painter.fillRect(QRect(0, 0, 40, 40), QColor(255, 0, 0));
-    painter.drawText(50, 50, "Have some text, my dear.");
+
+    if (curfile == NULL) {
+        drawNoFile(painter);
+    }
+    else {
+        element_offset = 0; // reset element start
+
+        /* Draw stuff */
+        drawAddressBar(painter);
+        drawHexContent(painter);
+        drawAsciiContent(painter);
+
+        /* Figure out how much area is taken up by the drawn area */
+        textarea_width = element_offset;
+    }
 }
 
 void HexEditor::resizeEvent(QResizeEvent *) {
+    adjust();
+}
 
+HexEditor::HexEditor(QWidget* parent) : QAbstractScrollArea(parent) {
+
+    /* Connect signals to slots */
+    connect(verticalScrollBar(), SIGNAL(valueChanged(int)), this, SLOT(adjust()));
+    connect(horizontalScrollBar(), SIGNAL(valueChanged(int)), this, SLOT(adjust()));
+
+    /* Initialize variables */
+    init();
 }
--- src/client/qt/hexeditor.h
+++ src/client/qt/hexeditor.h
@@ -2,20 +2,70 @@
 #define HEXEDITOR_H
 
 #include <QAbstractScrollArea>
-
+#include <mycroft/mdb.h>
+#include <mycroft/file.h>
 #include "hexeditor.h"
+
+//TODO: make these config variables
+#define QMC_HEXEDIT_ELEMENT_GAP 6
+#define QMC_HEXEDIT_BYTE_GAP 8
+#define QMC_HEXEDIT_BYTESPERROW 16
 
 class HexEditor : public QAbstractScrollArea {
 
     Q_OBJECT
 
+    /* Main internal funcs */
     void init();
 
+    /* File related */
+    mc_file_t* curfile;
+
+    /* Font related */
     void setFont(const QFont& font);
+
+    /* Section drawing */
+    void drawNoFile(QPainter& painter);
+    void drawAddressBar(QPainter& painter);
+    void drawHexContent(QPainter& painter);
+    void drawAsciiContent(QPainter& painter);
+
+    /* Spacing-related */
+    int element_offset;
+    int element_gap;
+
+    int rows_shown;
+
+    int font_cwidth;
+    int font_cheight;
+
+    int text_offset;
+    int row_offset;
+
+    int addrbar_offset;
+    int addrbar_width;
+    int addrbar_num_max;
+
+    int hexarea_offset;
+    int hexarea_width;
+    int hexarea_text_gap;
+
+    int asciiarea_offset;
+    int asciiarea_width;
+
+    int textarea_width = 0;
+    int textarea_rows = 0;
+    int textarea_rows_visible = 0;
 
 public:
 
-    HexEditor();
+    HexEditor(QWidget* parent = NULL);
+
+    int getNumLines();
+    void setCurLine(int pos);
+    void setCursor(int pos);
+
+    void setCurrentFile(mc_file_t* file);
 
 protected:
 
@@ -24,6 +74,10 @@ protected:
     void mousePressEvent(QMouseEvent* event);
     void paintEvent(QPaintEvent* event);
     void resizeEvent(QResizeEvent* event);
+
+private slots:
+
+    void adjust();
 };
 
 #endif // HEXEDITOR_H
