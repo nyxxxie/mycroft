ae28c3088e49bf43d2964fc463ebcc41c6216539
Nyxxie <nyxxie@protonmail.ch>
2016-09-23 16:30:18 -0400

    Attempting to get rid of the file tests requirement that we rerun make every time we want to test.

--- tests/core/test_file/main.cpp
+++ tests/core/test_file/main.cpp
@@ -1,6 +1,9 @@
 #include "gtest/gtest.h"
+#include "config.h"
+#include <stdio.h>
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
+    printf("%s\n", TEST_FILE_RES);
     return RUN_ALL_TESTS();
 }
