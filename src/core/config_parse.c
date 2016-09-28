622678d2bc27793b3f210064798f6b331cde0a11
Nyxxie <nyxxie@protonmail.ch>
2016-09-28 10:28:45 -0500

    This component isn't actually used and will be replaced later, removing it

--- src/core/config_parse.c
+++
@@ -1,5 +0,0 @@
-#include "config_parse.h"
-
-int config_parse(FILE* fp, mc_config_t* config) {
-
-}
--- src/core/config_parse.h
+++
@@ -1,9 +0,0 @@
-#ifndef MYCROFT_INT_CONFIG_PARSE_H
-#define MYCROFT_INT_CONFIG_PARSE_H
-
-#include <stdio.h>
-#include "cfg.h"
-
-int config_parse(FILE* fp, mc_config_t* config);
-
-#endif // MYCROFT_INT_CONFIG_PARSE_H
