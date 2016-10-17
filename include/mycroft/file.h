5e6fc38b594f3a5cd11f46a6adc58b6e47a9e50b
Nyxxie <nyxxie@protonmail.ch>
2016-10-16 23:54:00 -0500

    Modified file library to fit standard naming convention and standard initialization method.

--- include/mycroft/file.h
+++ include/mycroft/file.h
@@ -17,36 +17,36 @@ typedef struct mc_file_t mc_file_t;
 int file_exists(const char* file);
 
 /* Init/free functions */
-int file_init(mc_file_t* file);
-int file_open(mc_file_t* file, const char* file_name);
-int file_close(mc_file_t* file);
-int file_reload(mc_file_t* file);
+mc_file_t* mc_file_create();
+mc_file_t* mc_file_open(const char* file_name);
+int mc_file_close(mc_file_t* file);
+int mc_file_reload(mc_file_t* file);
 
 /* Template functions */
-template_t* file_get_template(mc_file_t* file);
-int file_set_template(mc_file_t* file, template_t* t);
+template_t* mc_file_get_template(mc_file_t* file);
+int mc_file_set_template(mc_file_t* file, template_t* t);
 
 /* Management functions */
-int file_get_cursor(mc_file_t* file);
-int file_set_cursor(mc_file_t* file, int cursor);
-fsize_t file_size(mc_file_t* file);
-char* file_name(mc_file_t* file);
-char* file_path(mc_file_t* file);
+int mc_file_get_cursor(mc_file_t* file);
+int mc_file_set_cursor(mc_file_t* file, int cursor);
+fsize_t mc_file_size(mc_file_t* file);
+char* mc_file_name(mc_file_t* file);
+char* mc_file_path(mc_file_t* file);
 
 /* Cache functions */
-int file_cache_init(mc_file_t* file, int size);
-int file_cache_loadzone(mc_file_t* file, int cursor);
-int file_cache_reload(mc_file_t* file);
+int mc_file_cache_init(mc_file_t* file, int size);
+int mc_file_cache_loadzone(mc_file_t* file, int cursor);
+int mc_file_cache_reload(mc_file_t* file);
 
 /* Read/write functions */
-int file_read(mc_file_t* file, fsize_t amount, uint8_t* outbuf);
-int file_read_raw(mc_file_t* file, fsize_t amount, uint8_t* outbuf);
-int file_write(mc_file_t* file, fsize_t amount, uint8_t* outbuf);
-int file_read_value(mc_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf);
-int file_write_value(mc_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf);
-int file_write_insert(mc_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf);
-int file_read_cache(mc_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf);
-int file_read_editor_area(mc_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf);
+int mc_file_read(mc_file_t* file, fsize_t amount, uint8_t* outbuf);
+int mc_file_read_raw(mc_file_t* file, fsize_t amount, uint8_t* outbuf);
+int mc_file_write(mc_file_t* file, fsize_t amount, uint8_t* outbuf);
+int mc_file_read_value(mc_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf);
+int mc_file_write_value(mc_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf);
+int mc_file_write_insert(mc_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf);
+int mc_file_read_cache(mc_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf);
+int mc_file_read_editor_area(mc_file_t* file, fsize_t offset, fsize_t amount, uint8_t* outbuf);
 
 #ifdef __cplusplus
 }
