3570df53b78227face27342f8fd54618cb85ac12
Nyxxie <nyxxie@protonmail.ch>
2016-10-19 22:43:01 -0500

    Added some tests for core.

--- tests/core/test_core/core.cpp
+++ tests/core/test_core/core.cpp
@@ -1,6 +1,49 @@
 #include <gtest/gtest.h>
 #include <mycroft/mycroft.h>
 
+TEST(context, create_ctx) {
+    mc_ctx_t* ctx = mc_ctx_create();
+    ASSERT_TRUE(ctx != NULL);
+}
+
+TEST(context, create_and_free_ctx) {
+    mc_ctx_t* ctx = mc_ctx_create();
+    ASSERT_TRUE(ctx != NULL);
+    mc_ctx_free(ctx);
+}
+
+TEST(project, create_project) {
+    mc_ctx_t* ctx = mc_ctx_create();
+    ASSERT_TRUE(ctx != NULL);
+    mc_ctx_free(ctx);
+}
+
+TEST(project, create_and_free_project) {
+    mc_ctx_t* ctx = mc_ctx_create();
+    ASSERT_TRUE(ctx != NULL);
+    mc_ctx_free(ctx);
+}
+
+TEST(project_new, add_project_to_ctx) {
+    mc_ctx_t* ctx = mc_ctx_create();
+    ASSERT_TRUE(ctx != NULL);
+
+    mc_project_t* project = mc_project_create("name");
+    mc_ctx_add_project(ctx, project);
+
+
+    mc_ctx_free(ctx);
+}
+
+TEST(project_new, add_many_projects_to_ctx) {
+}
+
+TEST(project_new, verify_project_is_stored) {
+}
+
+TEST(project_new, verify_projects_are_stored) {
+}
+
 //TEST(core_basic, mycroft_init_and_free) {
 //    mc_ctx_t* ctx = mycroft_init();
 //    ASSERT_TRUE(ctx != NULL);
