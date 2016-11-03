dcf51dfd92b2a05cc93b209031e487ba226d7266
Nyxxie <nyxxie@protonmail.ch>
2016-11-03 10:55:03 -0500

    Added documentation to mc_project_t.

--- lib/core/project.c
+++ lib/core/project.c
@@ -4,25 +4,34 @@
 #include "project.h"
 #include "file.h"
 
+/** @internal Number of initialization queries to run */
 #define QUERYNO 3
+
+/**
+ * Creates the tables in a newly created mycroft database.
+ *
+ * @param db sqlite3 database to operate on.
+ * @return mc_error_t code indicating success or failure.
+ * @internal
+ */
 mc_error_t create_default_tables(sqlite3* db) {
     const char* create_queries[QUERYNO] = {
         /* This table stores project information like name, date created, date
-         * last modified, etc... */
+           last modified, etc... */
         "create table project_info("
         "key       text  unique,"
         "value     blob);",
 
         /* This table stores the files opened by this project.  Please note
-         * the file attribute is the name of the file (including extension),
-         * whereas path contains the full path to the file. */
+           the file attribute is the name of the file (including extension),
+           whereas path contains the full path to the file. */
         "create table files("
         "file      text  unique,"
         "path      text);",
 
         /* This stores generic metadata about a file.  More detailed metadata
-         * can be found in other tables using the datatype specified (for
-         * example, comments) . */
+           can be found in other tables using the datatype specified (for
+           example, comments) . */
         "create table metadata("
         "byte_num  integer,"
         "datatype  integer,"
@@ -59,6 +68,12 @@ mc_error_t create_default_tables(sqlite3* db) {
     return MC_OK;
 }
 
+/**
+ * Create new project with a given name.
+ *
+ * @param name Name for new project
+ * @return Pointer to mc_project_t that was just created.
+ */
 mc_project_t* mc_project_create(const char* name) {
     mc_project_t* project = NULL;
     int rc = 0;
@@ -110,6 +125,12 @@ mc_project_t* mc_project_create(const char* name) {
     return project;
 }
 
+/**
+ * Loads project from an mdb file.
+ *
+ * @param mdb_file Path to mdb file to load.
+ * @return Pointer to mc_project_t that was loaded.
+ */
 mc_project_t* mc_project_load(const char* mdb_file) {
     mc_project_t* project = NULL;
 
@@ -129,6 +150,13 @@ mc_project_t* mc_project_load(const char* mdb_file) {
     return NULL;
 }
 
+/**
+ * Saves project to a file.
+ *
+ * @param project mc_project_t to save.
+ * @param mdb_file File to save to.  Can be a file that exists or doesn't.
+ * @return mc_error_t indicating success or failure.
+ */
 mc_error_t mc_project_save(mc_project_t* project, const char* mdb_file) {
     int rc = 0;
     sqlite3* savefile = NULL;
@@ -160,6 +188,11 @@ mc_error_t mc_project_save(mc_project_t* project, const char* mdb_file) {
     return MC_OK;
 }
 
+/**
+ * Free project.  This MUST be called when you are done with an mc_project_t.
+ *
+ * @param project mc_project_t to free.
+ */
 void mc_project_free(mc_project_t* project) {
     if (project != NULL) {
         /* Free file name */
@@ -188,6 +221,14 @@ void mc_project_free(mc_project_t* project) {
     }
 }
 
+/**
+ * Adds mc_file_t to sqlite3 database.
+ *
+ * @param db sqlite3 database to operate on.
+ * @param file mc_file_t to add to the database.
+ * @return mc_error_t indicating success or failure.
+ * @internal
+ */
 mc_error_t db_add_file(sqlite3* db, mc_file_t* file) {
     int rc = 0;
     sqlite3_stmt* stmt = NULL;
@@ -224,6 +265,13 @@ mc_error_t db_add_file(sqlite3* db, mc_file_t* file) {
     return MC_OK;
 }
 
+/**
+ * Adds a file to the project.
+ *
+ * @param project mc_project_t to add file to.
+ * @param file mc_file_t to add to project.
+ * @return mc_error_t indicating success or failure.
+ */
 mc_error_t mc_project_add_file(mc_project_t* project, mc_file_t* file) {
     mc_error_t rc = 0;
     uint32_t cur_index = 0;
@@ -270,6 +318,13 @@ mc_error_t mc_project_add_file(mc_project_t* project, mc_file_t* file) {
     return MC_OK;
 }
 
+/**
+ * Removes project from file.
+ *
+ * @param project mc_project_t to remove file from.
+ * @param file_index index of file to remove.
+ * @return mc_error_t indicating success or failure.
+ */
 mc_error_t mc_project_remove_file(mc_project_t* project, uint32_t file_index) {
 
     /* Ensure that the index is valid */
@@ -306,6 +361,13 @@ mc_error_t mc_project_remove_file(mc_project_t* project, uint32_t file_index) {
     return MC_OK;
 }
 
+/**
+ * Retrieves file from project associated with the given file_index.
+ *
+ * @param project mc_project_t to get file from.
+ * @param file_index index of file in project.
+ * @return Pointer to mc_file_t that cooresponds with file_index (or NULL).
+ */
 mc_file_t* mc_project_get_file(mc_project_t* project, uint32_t file_index) {
 
     if (project->files == NULL ||
@@ -316,10 +378,23 @@ mc_file_t* mc_project_get_file(mc_project_t* project, uint32_t file_index) {
     return project->files[file_index];
 }
 
+/**
+ * Returns amount of files in project.
+ *
+ * @param project mc_project_t to get file amount from.
+ * @return Amount of files in project.
+ */
 uint32_t mc_project_get_file_amount(mc_project_t* project) {
     return project->file_amt;
 }
 
+/**
+ * Focuses a file.
+ *
+ * @param project mc_project_t to set focused file in.
+ * @param file mc_file_t to set as focused.  Must be added to the project.
+ * @return mc_error_t indicating success or failure.
+ */
 mc_error_t mc_project_set_focused_file(mc_project_t* project, mc_file_t* file) {
     uint32_t i = 0;
 
@@ -336,6 +411,12 @@ mc_error_t mc_project_set_focused_file(mc_project_t* project, mc_file_t* file) {
     return MC_ERR;
 }
 
+/**
+ * Gets the currently focused file.
+ *
+ * @param project mc_project_t to get focused file from.
+ * @return Pointer to mc_file_t that is focused.
+ */
 mc_file_t* mc_project_get_focused_file(mc_project_t* project) {
     return project->file_focused;
 }
