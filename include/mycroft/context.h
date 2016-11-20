f46146a84a5260eaf845c082e91396bd4e6917b7
Nyxxie <nyxxie@protonmail.ch>
2016-11-20 01:31:00 -0600

    Moved context stuff to a dedicated header.

---
+++ include/mycroft/context.h
@@ -0,0 +1,30 @@
+#ifndef MYCROFT_CONTEXT_H
+#define MYCROFT_CONTEXT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <mycroft/context.h>
+#include <mycroft/project.h>
+
+typedef struct mc_ctx_t mc_ctx_t;
+
+/* Create/free */
+mc_ctx_t* mc_ctx_create();
+void mc_ctx_free(mc_ctx_t* ctx);
+
+/* Context management */
+uint32_t mc_ctx_add_project(mc_ctx_t* ctx, mc_project_t* project);
+int mc_ctx_remove_project(mc_ctx_t* ctx, uint32_t project_index);
+mc_project_t* mc_ctx_get_project(mc_ctx_t* ctx, uint32_t project_index);
+uint32_t mc_ctx_get_project_amount(mc_ctx_t* ctx);
+void mc_ctx_set_focused_project(mc_ctx_t* ctx, mc_project_t* project);
+mc_project_t* mc_ctx_get_focused_project(mc_ctx_t* ctx);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MYCROFT_CONTEXT_H
