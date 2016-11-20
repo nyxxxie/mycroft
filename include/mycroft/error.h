c8320e1eb2941b4d2a3c8fbe914dd9e90834c3c0
Nyxxie <nyxxie@protonmail.ch>
2016-11-20 01:17:30 -0600

    Moved logging functions to main include file.

--- include/mycroft/error.h
+++ include/mycroft/error.h
@@ -7,4 +7,17 @@ typedef enum {
     MC_OK
 } mc_error_t;
 
+// Optional arguments
+#define __OARGS(...) , ##__VA_ARGS__
+
+// Generic error output function
+#define MC_ERROR(fmt, ...) \
+    fprintf(stderr, "ERROR: %s:%i - ",  __FILE__, __LINE__); \
+    fprintf(stderr, fmt __OARGS(__VA_ARGS__));
+
+// Generic debug output function
+#define MC_DEBUG(fmt, ...) \
+    fprintf(stdout, "DEBUG: %s:%i - ",  __FILE__, __LINE__); \
+    fprintf(stdout, fmt __OARGS(__VA_ARGS__));
+
 #endif // MYCROFT_ERROR_H
