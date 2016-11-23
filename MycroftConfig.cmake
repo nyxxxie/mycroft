7af190bdd54c4631f493cb72840417bebd880a43
Nyxxie <nyxxie@protonmail.ch>
2016-11-23 13:37:18 -0600

    Moved core to mycroft_core.

--- MycroftConfig.cmake
+++ MycroftConfig.cmake
@@ -2,9 +2,8 @@
 set(MYCROFT_NAME mycroft)
 
 # Define target names.  THESE MUST MATCH UP WITH DIRECTORIES.
-set(MYCROFT_CLIENT_QT_NAME ${MYCROFT_NAME}-qt)
-set(MYCROFT_CLIENT_CLI_NAME ${MYCROFT_NAME}-cli)
-set(MYCROFT_CORE_NAME core)
+set(MYCROFT_CLIENT_QT_NAME q${MYCROFT_NAME})
+set(MYCROFT_CORE_NAME ${MYCROFT_NAME}_core)
 
 # Define finished binary file names
 set(MYCROFT_CLIENT_BIN ${MYCROFT_CLIENT_NAME})
@@ -24,51 +23,49 @@ else ()
     set(MYCROFT_LOCAL_BASE $ENV{HOME}/.${MAIN_PROJECT_NAME} CACHE PATH "mycroft local directory")
 endif ()
 
-# Sys base directory.  Used to store global data.
-if (DEFINED MYCROFT_SYS_BASE)
-    set(MYCROFT_SYS_BASE ${MYCROFT_SYS_BASE} CACHE PATH "mycroft sys directory")
-else ()
-    set(MYCROFT_SYS_BASE ${CMAKE_INSTALL_PREFIX} CACHE PATH "mycroft sys directory")
-endif ()
-
 # Set sys data directory
 if (DEFINED MYCROFT_DATA_PATH)
-    set(MYCROFT_DATA_PATH ${MYCROFT_DATA_PATH} CACHE PATH "mycroft sys data directory")
+    set(MYCROFT_DATA_PATH ${MYCROFT_DATA_PATH} CACHE PATH "mycroft data directory")
 else ()
-    set(MYCROFT_DATA_PATH ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_AWE_NAME} CACHE PATH "mycroft sys data directory")
+    set(MYCROFT_DATA_PATH ${CMAKE_INSTALL_PREFIX}/share/${MYCROFT_NAME} CACHE PATH "mycroft sys data directory")
 endif ()
 
 # Set sys data directory
 if (DEFINED MYCROFT_DOC_PATH)
-    set(MYCROFT_DOC_PATH ${MYCROFT_DOC_PATH} CACHE PATH "mycroft sys doc directory")
+    set(MYCROFT_DOC_PATH ${MYCROFT_DOC_PATH} CACHE PATH "mycroft doc directory")
 else ()
-    set(MYCROFT_DOC_PATH ${CMAKE_INSTALL_PREFIX}/share/doc/${PROJECT_AWE_NAME} CACHE PATH "mycroft sys doc directory")
+    set(MYCROFT_DOC_PATH ${CMAKE_INSTALL_PREFIX}/share/doc/${MYCROFT_NAME} CACHE PATH "mycroft sys doc directory")
 endif ()
 
 # Set sys data directory
 if (DEFINED MYCROFT_LIB_PATH)
-    set(MYCROFT_LIB_PATH ${MYCROFT_LIB_PATH} CACHE PATH "mycroft sys lib directory")
+    set(MYCROFT_LIB_PATH ${MYCROFT_LIB_PATH} CACHE PATH "mycroft lib directory")
 else ()
-    set(MYCROFT_LIB_PATH ${CMAKE_INSTALL_PREFIX}/lib CACHE PATH "mycroft sys lib directory")
+    set(MYCROFT_LIB_PATH ${CMAKE_INSTALL_PREFIX}/lib CACHE PATH "mycroft lib directory")
+endif ()
+
+# Set sys data directory
+if (DEFINED MYCROFT_BIN_PATH)
+    set(MYCROFT_BIN_PATH ${MYCROFT_BIN_PATH} CACHE PATH "mycroft bin directory")
+else ()
+    set(MYCROFT_BIN_PATH ${CMAKE_INSTALL_PREFIX}/bin CACHE PATH "mycroft bin directory")
 endif ()
 
 # Set some local directories and files
-set(MYCROFT_LOCAL_CONFIG   ${MYCROFT_LOCAL_BASE}/rc.py)     # config file
-set(MYCROFT_LOCAL_PLUGINS  ${MYCROFT_LOCAL_BASE}/plugins)   # plugin directory
-set(MYCROFT_LOCAL_SCRIPTS  ${MYCROFT_LOCAL_BASE}/scripts)   # simple scripts
-set(MYCROFT_LOCAL_MODULES  ${MYCROFT_LOCAL_BASE}/modules)   # non-plugin python or c modules
-set(MYCROFT_LOCAL_TEMPLATE ${MYCROFT_LOCAL_BASE}/templates) # template directory
-set(MYCROFT_LOCAL_TYPES    ${MYCROFT_LOCAL_BASE}/types)     # template directory
-
-# Set some local directories and files
-set(MYCROFT_CONFIG   ${MYCROFT_DATA_PATH}/rc.py)     # config file
-set(MYCROFT_PLUGINS  ${MYCROFT_DATA_PATH}/plugins)   # plugin directory
-set(MYCROFT_SCRIPTS  ${MYCROFT_DATA_PATH}/scripts)   # simple scripts
-set(MYCROFT_MODULES  ${MYCROFT_DATA_PATH}/modules)   # non-plugin python or c modules
-set(MYCROFT_TEMPLATE ${MYCROFT_DATA_PATH}/templates) # template directory
-set(MYCROFT_TYPES    ${MYCROFT_DATA_PATH}/types)     # template directory
+set(MYCROFT_INSTALL_PLUGIN_PATH   ${MYCROFT_DATA_PATH}/plugins)
+set(MYCROFT_INSTALL_SCRIPT_PATH   ${MYCROFT_DATA_PATH}/scripts)
+set(MYCROFT_INSTALL_TEMPLATE_PATH ${MYCROFT_DATA_PATH}/templates)
+set(MYCROFT_INSTALL_TYPE_PATH     ${MYCROFT_DATA_PATH}/types)
+set(MYCROFT_INSTALL_LIB_CORE      ${MYCROFT_LIB_PATH}/core)
+set(MYCROFT_INSTALL_CLIENT_QT     ${MYCROFT_LIB_PATH}/bin)
 
 # Set project directory locations
-set(MYCROFT_CORE_DIR   ${CMAKE_SOURCE_DIR}/lib/core)
-set(MYCROFT_CLIENT_DIR ${CMAKE_SOURCE_DIR}/src/client)
-set(MYCROFT_TEST_DIR   ${CMAKE_SOURCE_DIR}/tests)
+set(MYCROFT_LOCAL_PLUGIN_PATH   ${CMAKE_SOURCE_DIR}/res/plugins)
+set(MYCROFT_LOCAL_SCRIPT_PATH   ${CMAKE_SOURCE_DIR}/res/scripts)
+set(MYCROFT_LOCAL_TEMPLATE_PATH ${CMAKE_SOURCE_DIR}/res/templates)
+set(MYCROFT_LOCAL_TYPES_PATH    ${CMAKE_SOURCE_DIR}/res/types)
+set(MYCROFT_LOCAL_LIB_CORE      ${CMAKE_SOURCE_DIR}/core)
+set(MYCROFT_LOCAL_CLIENT_QT     ${CMAKE_SOURCE_DIR}/client/qt)
+set(MYCROFT_LOCAL_CORE_PATH     ${CMAKE_SOURCE_DIR}/lib/core)
+set(MYCROFT_LOCAL_CLIENT_PATH   ${CMAKE_SOURCE_DIR}/src/client)
+set(MYCROFT_LOCAL_TEST_PATH     ${CMAKE_SOURCE_DIR}/tests)
