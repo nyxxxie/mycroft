fbd959c8b8b5709aa7a1161ebcce834d6581f091
Nyxxie <nyxxie@protonmail.ch>
2016-12-17 04:15:45 -0600

    Compressed templatemodel and templateeditor into one file.

--- src/client/qt/templateeditor.h
+++ src/client/qt/templateeditor.h
@@ -2,9 +2,36 @@
 #define TEMPLATEEDITOR_H
 
 #include <QString>
+#include <QVariant>
 #include <QTreeView>
-#include <mycroft/mycroft.h>
-#include "templatemodel.h"
+#include <QModelIndex>
+#include <QAbstractItemModel>
+#include <mycroft/template.h>
+
+class TemplateEditor;
+
+class TemplateModel : public QAbstractItemModel
+{
+    Q_OBJECT
+
+    TemplateEditor* p;
+    bool shouldRender() const;
+    ast_struct_t* getParent(const QModelIndex& parent) const;
+
+public:
+    explicit TemplateModel(TemplateEditor* parent);
+    ~TemplateModel();
+
+    QVariant data(const QModelIndex& index, int role) const Q_DECL_OVERRIDE;
+    Qt::ItemFlags flags(const QModelIndex& index) const Q_DECL_OVERRIDE;
+    QVariant headerData(int section, Qt::Orientation orientation,
+                        int role = Qt::DisplayRole) const Q_DECL_OVERRIDE;
+    QModelIndex index(int row, int column,
+                      const QModelIndex& parent = QModelIndex()) const Q_DECL_OVERRIDE;
+    QModelIndex parent(const QModelIndex& index) const Q_DECL_OVERRIDE;
+    int rowCount(const QModelIndex& parent = QModelIndex()) const Q_DECL_OVERRIDE;
+    int columnCount(const QModelIndex& parent = QModelIndex()) const Q_DECL_OVERRIDE;
+};
 
 class TemplateEditor : public QTreeView
 {
