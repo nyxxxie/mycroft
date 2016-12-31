227dab673e204c3da7281905d686d74c705a7e0a
Nyxxie <nyxxie@protonmail.ch>
2016-12-31 17:10:17 -0600

    Removed type.* files and also got rid of a bunch of default types I don't plan on implementing in C

--- lib/mycroft_core/template/types/char.c
+++ lib/mycroft_core/template/types/char.c
@@ -1,13 +1,11 @@
-#include "template/type.h"
+#include "template/typedef.h"
 
-mc_error_t from_string(mc_type_t* type, const char* value)
+mc_error_t to_string(mc_typedef_t* type, uint8_t* bytes, size_t byte_amount, char** strbuf, size_t* strbuf_size)
 {
-    return MC_OK;
+    return MC_ERR;
 }
 
-char* to_string(mc_type_t* type)
+mc_error_t from_string(mc_typedef_t* type, const char* string, uint8_t** bytes, size_t* byte_amount)
 {
-    return NULL;
+    return MC_ERR;
 }
-
-MC_TYPE_REGISTER_BUILTIN("char", to_string, from_string);
