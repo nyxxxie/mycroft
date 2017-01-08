2732f9d0c6dded98e22d4516b75a39e9b321aa01
Nyxxie <nyxxie@protonmail.ch>
2017-01-07 23:44:49 -0600

    Forgot to add extern c blocks around this header's functions.

--- include/mycroft/script.h
+++ include/mycroft/script.h
@@ -1,9 +1,17 @@
 #ifndef MYCROFT_SCRIPT_H
 #define MYCROFT_SCRIPT_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <mycroft/error.h>
 
 mc_error_t mc_script_runfile(const char* path);
 mc_error_t mc_script_runstring(const char* script);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // MYCROFT_SCRIPT_H
