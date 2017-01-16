d8ac32890cf56982e85c43691de3579daa3730e2
Constantine <t3a@protonmail.com>
2017-01-16 03:08:15 +0200

    Correct largefile test

--- tests/core/file/largefile/largefile.cpp
+++ tests/core/file/largefile/largefile.cpp
@@ -13,8 +13,8 @@
 TEST(file_largefile, file_seeking) {
     mc_file_t* f = mc_file_open("largesparsefile");
     ASSERT_TRUE(f != NULL);
-    ASSERT_EQ(mc_file_set_cursor(f, (fsize_t) 0x100000000), 0);
-    ASSERT_EQ(mc_file_close(f), 0);
+    ASSERT_EQ(mc_file_set_cursor(f, (fsize_t) 0x100000000), MC_OK);
+    ASSERT_EQ(mc_file_close(f), MC_OK);
 }
 
 
@@ -25,8 +25,8 @@ TEST(file_largefile, file_seeking) {
     mc_file_t* f = mc_file_open("largesparsefile");
     uint8_t outbuf[6];
     ASSERT_TRUE(f != NULL);
-    ASSERT_EQ(mc_file_set_cursor(f, (fsize_t) 0x100000000), 0);
-    ASSERT_EQ(mc_file_read(f, sizeof(outbuf), outbuf), 0);
+    ASSERT_EQ(mc_file_set_cursor(f, (fsize_t) 0x100000000), MC_OK);
+    ASSERT_EQ(mc_file_read(f, sizeof(outbuf), outbuf, NULL), MC_OK);
     ASSERT_EQ(memcmp("hello!", outbuf, sizeof(outbuf)), 0);
-    ASSERT_EQ(mc_file_close(f), 0);
+    ASSERT_EQ(mc_file_close(f), MC_OK);
 }
